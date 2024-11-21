10d3063e2b15e226fdb1b8bcdec110b0c35081a9
Sam Boynton <samlingx@gmail.com>
2024-11-20 17:29:48 -0800

    Commit script for testing

--- tools/milvus-rag/milvus-rag.py
+++ tools/milvus-rag/milvus-rag.py
@@ -7,6 +7,8 @@ license: MIT
 requirements: haystack-ai,pymilvus==2.4.9,milvus-haystack
 """
 
+import asyncio
+import os
 import json
 from pydantic import BaseModel, Field
 from typing import Callable, Any
@@ -14,6 +16,7 @@ from haystack import Pipeline
 from haystack.components.embedders import OpenAITextEmbedder
 from milvus_haystack import MilvusDocumentStore
 from milvus_haystack.milvus_embedding_retriever import MilvusEmbeddingRetriever
+
 
 class EventEmitter:
     def __init__(self, event_emitter: Callable[[dict], Any] = None):
@@ -32,14 +35,15 @@ class EventEmitter:
                 }
             )
 
+
 class Tools:
     class Valves(BaseModel):
         MILVUS_ENDPOINT: str = Field(
-            default="http://milvus:19530",
+            default="http://milvus.milvus:19530",
             description="The Milvus endpoint to query.",
         )
         MILVUS_OPENAI_BASE_URL: str = Field(
-            default="https://api.openai.com",
+            default="http://litellm.litellm:4000",
             description="The OpenAI base endpoint.",
         )
         MILVUS_OPENAI_API_KEY: str = Field(
@@ -62,14 +66,14 @@ class Tools:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         }
 
-    async def query_milvus(
+    async def query(
         self,
         query: str,
         __event_emitter__: Callable[[dict], Any] = None,
     ) -> str:
         """
-        Send a search query to Milvus and get the content of the result. Search documents, images, pdfs, CSVs, etc.
-        :params query: Search query sent to Milvus.
+        Search the database and send the contents of the result. Search for unknown knowledge, news, info, public contact info, weather, etc..
+        :params query: Query sent to Milvus.
         :return: The content of the response.
         """
         try:
@@ -77,52 +81,72 @@ class Tools:
 
             await emitter.emit(f"Initiating RAG search for: {query}")
 
-            prompt_template = """Answer the following query based on the provided context. If the context does
-                not include an answer, reply with {{documents}}.\n
-                Query: {{query}}
-                Documents:
-                {% for doc in documents %}
-                {{ doc.content }}
-                {% endfor %}
-                Answer: 
-            """
+            print(f"ENDPOINT: {self.valves.MILVUS_ENDPOINT}")
 
             milvus_endpoint = self.valves.MILVUS_ENDPOINT
+            milvus_openai_base_url = self.valves.MILVUS_OPENAI_BASE_URL
+            milvus_openai_api_key = self.valves.MILVUS_OPENAI_API_KEY
             milvus_openai_text_embedder = self.valves.MILVUS_OPENAI_TEXT_EMBEDDER
             milvus_top_k = self.valves.MILVUS_TOP_K
 
+            print("WE ARE RIGHT BEFORE THE DOCUMENT STORE INIT")
             document_store = MilvusDocumentStore(
                 connection_args={"uri": milvus_endpoint}
             )
-
+            print("WE ARE RIGHT BEFORE THE PIPELINE INIT")
             rag_pipeline = Pipeline()
-            rag_pipeline.add_component("text_embedder", OpenAITextEmbedder(model=milvus_openai_text_embedder))
-            rag_pipeline.add_component("retriever", MilvusEmbeddingRetriever(document_store=document_store, top_k=milvus_top_k))
+            print("WE ARE RIGHT BEFORE THE ADD COMPONENT")
+            rag_pipeline.add_component(
+                "text_embedder",
+                OpenAITextEmbedder(
+                    model="text-embedding-3-small",
+                    api_base_url="",
+                    api_key="",
+                ),
+            )
+            # print("WE ARE RIGHT BEFORE THE ADD COMPONENT 2")
+            # rag_pipeline.add_component(
+            #     "retriever",
+            #     MilvusEmbeddingRetriever(
+            #         document_store=document_store, top_k=milvus_top_k
+            #     ),
+            # )
 
-            rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+            # print("WE ARE RIGHT BEFORE THE PIPELINE CONNECT")
+            # rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
 
-            try:
-                await emitter.emit("Sending request to Milvas")
-                
-                results = rag_pipeline.run(
-                    {
-                        "text_embedder": {"text": query},
-                        "prompt_builder": {"query": query}
-                    }
-                )
-                await emitter.emit(f"Retrieved query results")
-                return json.dumps(results['retriever']['documents'])
+            # print("WE ARE RIGHT BEFORE THE TRY BLOCK")
+            # try:
+            #     await emitter.emit("Sending request to Milvas")
+            #     print("WE ARE HERE")
 
-            except Exception as e:
-                await emitter.emit(
-                    status="error",
-                    description=f"Error during search: {str(e)}",
-                    done=True,
-                )
-                return json.dumps({"error": str(e)})
+            #     results = rag_pipeline.run(
+            #         {
+            #             "text_embedder": {"text": query},
+            #         }
+            #     )
+            #     await emitter.emit(f"Retrieved query results")
+            #     return {"results": results["retriever"]["documents"]}
 
-       
+            # except Exception as e:
+            #     await emitter.emit(
+            #         status="error",
+            #         description=f"Error during search: {str(e)}",
+            #         done=True,
+            #     )
+            #     return json.dumps({"error": str(e)})
+
         except Exception as e:
             return {
                 "content": f"Unexpected error occurred. Error: {str(e)}",
             }
+
+async def main():
+    tools = Tools()
+    results = await tools.query("What are all the transactions in October 2024?")
+ 
+    print(results)
+ 
+ 
+if __name__ == "__main__":
+    asyncio.run(main())
