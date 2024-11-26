5847a70ec0242bb1cb2d88cbe59797f3b6c78d86
Sam Boynton <samlingx@gmail.com>
2024-11-26 16:32:39 -0500

    Revert back to own filter

--- filters/langfuse-tags.py
+++ filters/langfuse-tags.py
@@ -1,152 +1,109 @@
 """
 title: Langfuse Filter Pipeline
 author: open-webui
-date: 2024-09-27
-version: 1.4
+date: 2024-05-30
+version: 1.1
 license: MIT
 description: A filter pipeline that uses Langfuse.
 requirements: langfuse
 """
 
+from functools import cache
+from langfuse.api.resources.commons.errors.unauthorized_error import UnauthorizedError
+from pprint import pformat
 from typing import List, Optional
+import json
+import langfuse
 import os
 import uuid
-from pprint import pformat
 
-from utils.pipelines.main import get_last_assistant_message
 from pydantic import BaseModel
-from langfuse import Langfuse
-from langfuse.api.resources.commons.errors.unauthorized_error import UnauthorizedError
 
-def get_last_assistant_message_obj(messages: List[dict]) -> dict:
-    for message in reversed(messages):
-        if message["role"] == "assistant":
-            return message
-    return {}
-
+@cache
+def load_json_dict(user_value: str) -> dict:
+    user_value = user_value.strip()
+    if not user_value:
+        return {}
+    loaded = json.loads(user_value)
+    assert isinstance(loaded, dict), f"json is not a dict but '{type(loaded)}'"
+    return loaded
 
 class Pipeline:
     class Valves(BaseModel):
-        pipelines: List[str] = []
+        # Assign a priority level to the filter pipeline.
+        # The priority level determines the order in which the filter pipelines are executed.
+        # The lower the number, the higher the priority.
         priority: int = 0
+
+        pipelines: List[str] = []
+
+        # Valves
         secret_key: str
         public_key: str
         host: str
+        tags: List[str]
 
     def __init__(self):
+        # Initialize
         self.type = "filter"
-        self.name = "Langfuse Filter"
+        self.langfuse = None
         self.valves = self.Valves(
             **{
                 "pipelines": ["*"],
                 "secret_key": os.getenv("LANGFUSE_SECRET_KEY", "your-secret-key-here"),
                 "public_key": os.getenv("LANGFUSE_PUBLIC_KEY", "your-public-key-here"),
-                "host": os.getenv("LANGFUSE_HOST", "https://cloud.langfuse.com"),
+                "host": os.getenv("LANGFUSE_HOST", "https://us.cloud.langfuse.com"),
+                "tags": ["open-webui"]
             }
         )
-        self.langfuse = None
-        self.chat_generations = {}
 
     async def on_startup(self):
         print(f"on_startup:{__name__}")
         self.set_langfuse()
 
+    async def on_valves_updated(self):
+        # This function is called when the valves are updated.
+        self.set_langfuse()
+
     async def on_shutdown(self):
         print(f"on_shutdown:{__name__}")
         self.langfuse.flush()
-
-    async def on_valves_updated(self):
-        self.set_langfuse()
+        pass
 
     def set_langfuse(self):
         try:
-            self.langfuse = Langfuse(
+            self.langfuse = langfuse.Langfuse(
                 secret_key=self.valves.secret_key,
                 public_key=self.valves.public_key,
                 host=self.valves.host,
-                debug=False,
+                debug=False
             )
             self.langfuse.auth_check()
+            print("Connected to Langfuse")
         except UnauthorizedError:
-            print(
-                "Langfuse credentials incorrect. Please re-enter your Langfuse credentials in the pipeline settings."
-            )
+            print("Langfuse credentials incorrect.")
         except Exception as e:
-            print(f"Langfuse error: {e} Please re-enter your Langfuse credentials in the pipeline settings.")
+            print(f"Langfuse error: {str(e)}")
 
-    async def inlet(self, body: dict, user: Optional[dict] = None) -> dict:
+    async def inlet(
+        self,
+        body: dict,
+        user: Optional[dict] = None
+    ) -> dict:
+
+        # if "chat_id" not in body:
+        #     body["chat_id"] = uuid.uuid4()
+
+        body["tags"] = ["open-webui"]
+
+        async def log(message: str):
+            print(f"Inlet: {message}")
+
+        await log(f"Body: {pformat(body)}")
+
         print(f"inlet:{__name__}")
-        print(f"Received body: {body}")
-        print(f"User: {user}")
-
-        # Check for presence of required keys and generate chat_id if missing
-        if "chat_id" not in body:
-            unique_id = f"SYSTEM MESSAGE {uuid.uuid4()}"
-            body["chat_id"] = unique_id
-            print(f"chat_id was missing, set to: {unique_id}")
-
-        required_keys = ["model", "messages"]
-        missing_keys = [key for key in required_keys if key not in body]
-        
-        if missing_keys:
-            error_message = f"Error: Missing keys in the request body: {', '.join(missing_keys)}"
-            print(error_message)
-            raise ValueError(error_message)
-
-        trace = self.langfuse.trace(
-            name=f"filter:{__name__}",
-            input=body,
-            user_id=user["email"],
-            metadata={"user_name": user["name"], "user_id": user["id"]},
-            session_id=body["chat_id"],
-        )
-
-        generation = trace.generation(
-            name=body["chat_id"],
-            model=body["model"],
-            input=body["messages"],
-            metadata={"interface": "open-webui"},
-        )
-
-        self.chat_generations[body["chat_id"]] = generation
-        print(trace.get_trace_url())
-
         return body
 
-    async def outlet(self, body: dict, user: Optional[dict] = None) -> dict:
-        print(f"outlet:{__name__}")
-        print(f"Received body: {body}")
-        if body["chat_id"] not in self.chat_generations:
-            return body
-
-        generation = self.chat_generations[body["chat_id"]]
-        assistant_message = get_last_assistant_message(body["messages"])
-
-        
-        # Extract usage information for models that support it
-        usage = None
-        assistant_message_obj = get_last_assistant_message_obj(body["messages"])
-        if assistant_message_obj:
-            info = assistant_message_obj.get("info", {})
-            if isinstance(info, dict):
-                print(pformat(info))
-                input_tokens = info.get("prompt_eval_count") or info.get("prompt_tokens")
-                output_tokens = info.get("eval_count") or info.get("completion_tokens")
-                if input_tokens is not None and output_tokens is not None:
-                    usage = {
-                        "input": input_tokens,
-                        "output": output_tokens,
-                        "unit": "TOKENS",
-                    }
-
-        # Update generation
-        generation.end(
-            output=assistant_message,
-            metadata={"interface": "open-webui"},
-            usage=usage,
-        )
-
-        # Clean up the chat_generations dictionary
-        del self.chat_generations[body["chat_id"]]
-
-        return body
+    # async def outlet(self, body: dict, user: Optional[dict] = None) -> dict:
+    #     print(f"outlet:{__name__}")
+    #     return body
