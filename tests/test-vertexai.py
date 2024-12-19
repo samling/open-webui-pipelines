c31cde6b56398568305be7ea78225e8dc6a3e96c
Sam Boynton <samlingx@gmail.com>
2024-12-19 10:50:31 -0800

    Add citations for VertexAI grounding

--- tests/test-vertexai.py
+++ tests/test-vertexai.py
@@ -1,7 +1,8 @@
 from google import genai
 from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
+import os
 
-gemini_api_key = "my-key"
+gemini_api_key = os.environ['GEMINI_API_KEY']
 project_id = "gen-lang-client-0352636303"
 location = "us-central1"
 app_creds = "/home/sboynton/Documents/HomeLab/apps-internal/scripts/vertex_key.json"
@@ -28,11 +29,7 @@ response = client.models.generate_content_stream(
 for chunk in response:
     if chunk.text:
         print(chunk.text)
-    if chunk.candidates[0].grounding_metadata and chunk.candidates[0].grounding_metadata is not None:
-        for grounding_chunk in chunk.candidates[0].grounding_metadata.grounding_chunks:
-            print(grounding_chunk)
-print(response)
-# Print grounding metadata after streaming (if needed)
-if hasattr(chunk, 'grounding_metadata') and chunk.grounding_metadata:
-    print("\nGrounding metadata:")
-    print(chunk.candidates[0].grounding_metadata.search_entry_point.rendered_content)
+    if chunk.candidates is not None:
+        if chunk.candidates[0].grounding_metadata and chunk.candidates[0].grounding_metadata is not None:
+            for grounding_chunk in chunk.candidates[0].grounding_metadata.grounding_chunks:
+                print(grounding_chunk.web)
